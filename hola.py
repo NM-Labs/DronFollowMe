b7abc2898c1a35773576afcff0494202dcb130cf
Mariuki <mario_netta12@hotmail.com>
2021-02-11 17:31:20 -0600

    Probando

--- hola.py
+++ hola.py
@@ -1,14 +1,25 @@
-import numpy as np
+# # %% hola
+# import numpy as np
+# import pandas as pd
+# lista = [12,15,16]
+# num = np.array(lista)
+# print(num)
+#
+# # %% hola
+# dic = {'r': [5], 'm':[10]}
+# data = pd.DataFrame(dic)
+# print(data)
+
+# %%
 import pandas as pd
+# %%
+# Enable the table_schema option in pandas,
+# data-explorer makes this snippet available with the `dx` prefix:
+pd.options.display.html.table_schema = True
+pd.options.display.max_rows = None
 
-lista = [12,15,16]
+# (Your dataframe here)
+iris_filename = 'iris.csv'
+df1 = pd.read_csv(iris_filename)
 
-num = np.array(lista)
-
-print(num)
-
-dic = {'r': [5], 'm':[10]}
-
-data = pd.DataFrame(dic)
-
-print(data)
+df1
