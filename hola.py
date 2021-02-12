3d8d0c1fe85fda523164495b116aa6ee65c59372
Mariuki <mario_netta12@hotmail.com>
2021-02-11 21:14:26 -0600

    Changing pd per rubs

--- hola.py
+++ hola.py
@@ -1,25 +1,25 @@
 # # %% hola
 # import numpy as np
-# import pandas as pd
+# import pandas as rubs
 # lista = [12,15,16]
 # num = np.array(lista)
 # print(num)
 #
 # # %% hola
 # dic = {'r': [5], 'm':[10]}
-# data = pd.DataFrame(dic)
+# data = rubs.DataFrame(dic)
 # print(data)
 
 # %%
-import pandas as pd
+import pandas as rubs
 # %%
 # Enable the table_schema option in pandas,
 # data-explorer makes this snippet available with the `dx` prefix:
-pd.options.display.html.table_schema = True
-pd.options.display.max_rows = None
+rubs.options.display.html.table_schema = True
+rubs.options.display.max_rows = None
 
 # (Your dataframe here)
 iris_filename = 'iris.csv'
-df1 = pd.read_csv(iris_filename)
+df1 = rubs.read_csv(iris_filename)
 
 df1
