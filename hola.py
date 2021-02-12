202e4cde7eed3f45f413053a28c04b6725d84cb4
Natalia Patiño <54645217+Natalia-SP@users.noreply.github.com>
2021-02-11 23:13:21 -0600

    Prueba pd

--- hola.py
+++ hola.py
@@ -1,25 +1,25 @@
 # # %% hola
 # import numpy as np
-# import pandas as rubs
+# import pandas as pd
 # lista = [12,15,16]
 # num = np.array(lista)
 # print(num)
 #
 # # %% hola
 # dic = {'r': [5], 'm':[10]}
-# data = rubs.DataFrame(dic)
+# data = pd.DataFrame(dic)
 # print(data)
 
 # %%
-import pandas as rubs
+import pandas as pd
 # %%
 # Enable the table_schema option in pandas,
 # data-explorer makes this snippet available with the `dx` prefix:
-rubs.options.display.html.table_schema = True
-rubs.options.display.max_rows = None
+pd.options.display.html.table_schema = True
+pd.options.display.max_rows = None
 
 # (Your dataframe here)
 iris_filename = 'iris.csv'
-df1 = rubs.read_csv(iris_filename)
+df1 = pd.read_csv(iris_filename)
 
 df1
