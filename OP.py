c732d73e4885a0433ff02d108402ea8d5477b1e2
Natalia Patiño <54645217+Natalia-SP@users.noreply.github.com>
2021-02-17 19:30:32 -0600

    Params
    Co-Authored-By: Mario Rosas Otero <Mariuki@users.noreply.github.com>

--- OP.py
+++ OP.py
@@ -15,6 +15,36 @@ from matplotlib import pyplot as plt
 import time
 from math import pi, atan2, degrees, sqrt
 
+
+class Params:
+    @staticmethod
+    def set_params(face_detection=False, hand_detection=False):
+
+            params = dict()
+            params["logging_level"] = 3
+            params["output_resolution"] = "-1x-1"
+            params["net_resolution"] = "-1x368"
+            params["model_pose"] = "BODY_25"
+            params["alpha_pose"] = 0.6
+            params["scale_gap"] = 0.3
+            params["scale_number"] = 1
+            params["render_threshold"] = 0.05
+            # If GPU version is built, and multiple GPUs are available, set the ID here
+            params["num_gpu_start"] = 0
+            params["disable_blending"] = False
+            # Ensure you point to the correct path where models are located
+            params["model_folder"] = dir_path + "/../../../models/"
+
+            # if not self.openpose_rendering:
+            #     params["render_pose"] = 0
+            if face_detection:
+                params["face"] = True
+                params["face_render_threshold"] = 0.4
+            if hand_detection:
+                params["hand"] = True
+            return params
+
+
 class OP:
     @staticmethod
     def ccw(A,B,C):
